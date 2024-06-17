126f8bea8241528437a42b9f37316b3745b7264c
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-06-17 19:33:01 -0300

    tests

---
+++ src/test/java/edu/austral/ingsis/MyFileSystemRunner.java
@@ -0,0 +1,18 @@
+package edu.austral.ingsis;
+
+import edu.austral.ingsis.clifford.CommandLine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyFileSystemRunner implements FileSystemRunner {
+    @Override
+    public List<String> executeCommands(List<String> commands) {
+        CommandLine cli = new CommandLine();
+        ArrayList<String> output = new ArrayList<>();
+        for (String command : commands) {
+            output.add(cli.execute(command));
+        }
+        return output;
+    }
+}
