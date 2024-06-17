c98709d8b715dc436e44b248ea82c909983e73f6
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-06-17 14:28:05 -0300

    basic file system

--- src/main/java/edu/austral/ingsis/clifford/FileSystem.java
+++ src/main/java/edu/austral/ingsis/clifford/FileSystem.java
@@ -1,4 +1,33 @@
 package edu.austral.ingsis.clifford;
 
-public interface FileSystem {
+import edu.austral.ingsis.clifford.commands.Command;
+import edu.austral.ingsis.clifford.filesystem.Directory;
+import edu.austral.ingsis.clifford.filesystem.FileSystemItem;
+
+public class FileSystem {
+    // Receiver
+    private Directory currentDirectory;
+    private Command command;
+
+    public FileSystem(Command command) {
+        currentDirectory = new Directory("/");
+        this.command = command;
+    }
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void execute() {
+        command.execute();
+    }
+
+    public void setCurrentDirectory(Directory currentDirectory) {
+        this.currentDirectory = currentDirectory;
+    }
+
+    public Directory getCurrentDirectory() {
+        return currentDirectory;
+    }
+
 }
